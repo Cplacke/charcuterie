2381b6e4715f704ad8c776ca0410849104f24942
cplacke <colin.m.placke@optum.com>
2022-09-26 20:42:23 -0400

    add try catch to init

--- helpers/mailer.ts
+++ helpers/mailer.ts
@@ -37,10 +37,14 @@ export const sendMail = async ({
 }
 
 console.info('sending mail ...');
-await sendMail({ 
-    name: 'Colin Placke - TEST',
-    email: 'colin@placke.com',
-    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-})
+try { 
+    await sendMail({ 
+        name: 'Colin Placke - TEST',
+        email: 'colin@placke.com',
+        message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    })
+} catch (err) {
+    console.error(err);
+}
 
 console.info('MAIL SENT!');
